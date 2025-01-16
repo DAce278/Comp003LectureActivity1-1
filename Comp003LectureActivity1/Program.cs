3491b8ff359b5ab72c9813769247e4a5f9751a6d
DAce278 <deeganacevedo@my.whccd.edu>
2025-01-15 19:59:00 -0800

    Add project files.

---
+++ Comp003LectureActivity1/Program.cs
@@ -0,0 +1,13 @@
+﻿// namespace level
+namespace Comp003LectureActivity1
+{
+    // class level
+    internal class Program
+    {
+        // main level
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
